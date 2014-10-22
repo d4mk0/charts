5551647257194c42006c0d685ec430f9aeead6cf
Damir Konkov <d4mk0f@gmail.com>
2014-10-23 01:27:20 +0400

    temporary commit

--- app/models/data_item.rb
+++ app/models/data_item.rb
@@ -1,2 +1,42 @@
+#encoding: utf-8
 class DataItem < ActiveRecord::Base
+
+  def self.import(file)
+    file = "#{Rails.root}/data.csv"
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      data_item = DataItem.new
+      attrs = row.to_hash.slice('date', 'store', 'rate', 'criterion', 'status')
+      attrs['criterion'] = case attrs['criterion']
+      when 'Да'
+        true
+      when 'Нет'
+        false
+      else
+        nil
+      end
+      attrs['date'] = Date.strptime(row['date'], "%m/%d/%Y")
+      data_item.attributes = attrs
+      data_item.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file)
+    when ".csv" then Roo::CSV.new(file, csv_options: {col_sep: ";"})
+    when ".xls" then Roo::Excel.new(file)
+    when ".xlsx" then Roo::Excelx.new(file)
+    else raise "Unknown file type: #{file}"
+    end
+  end
+
+  def self.line_chart_by_stores
+    uniq.pluck(:store).sort.each do |s|
+      {name: s, data: where(store: s).group_by_day(:date).count}
+    end
+  end
+
+
 end
